178c25d844c3ed74680ddf6c95a9fe36f913374e
Cyborger1 <45152844+Cyborger1@users.noreply.github.com>
2021-06-06 22:16:06 -0400

    Setup project

--- src/test/java/com/puzzleboxremapping/PuzzleBoxRemappingPluginTest.java
+++ src/test/java/com/puzzleboxremapping/PuzzleBoxRemappingPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.puzzleboxremapping;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class PuzzleBoxRemappingPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(PuzzleBoxRemappingPlugin.class);
 		RuneLite.main(args);
 	}
 }
